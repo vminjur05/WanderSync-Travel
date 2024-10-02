cdd9f139e958f72237b071b4a433d7a13c4e6a79
Abhiram Chilakamarri <abhiramchilakamarri@gmail.com>
2024-10-02 16:02:13 -0400

    Added UI for welcome page and login page, and connected them with Start button

--- Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/MainActivity.java
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.sprintproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button startButton = findViewById(R.id.btnStart);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
