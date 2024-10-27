aedc74041985a9e181ff75d492d947e346af58dd
vminjur05 <vkminjur@gmail.com>
2024-10-27 19:39:44 -0400

    Edited the Logistics Fragment and added the LogisticsViewModel

---
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/viewmodels/LogisticsViewModel.java
@@ -0,0 +1,8 @@
+package com.example.sprintproject.viewmodels;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+public class LogisticsViewModel extends ViewModel {
+}
