24505b1261f0b8f811305dca043886ab7139bf6d
vminjur05 <vkminjur@gmail.com>
2024-11-24 18:56:17 -0500

    edited transport screen

--- Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/views/TransportationFragment.java
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/views/TransportationFragment.java
@@ -26,6 +26,8 @@ public class TransportationFragment extends Fragment {
 
     public TransportationFragment() {
         // Required empty public constructor
+
+        //not needed
     }
 
     /**
